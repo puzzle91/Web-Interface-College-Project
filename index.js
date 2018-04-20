32dcc973294e0e1f950a64aaa35b4b36f1b4e91c
Eduardo Parkinson de Castro <eduardop@tcd.ie>
2018-04-20 16:07:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+function myAlert() {
+
+    alert("Your message has been sent");
+}
+
+
+function onClick() {
+    var x = document.getElementById("hiddenDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
